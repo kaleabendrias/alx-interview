487e1ba60277ce2a129ca2dc2b54f81dc043517c
kaleab <kaleabendrias1212@gmail.com>
2024-01-09 12:48:55 +0300

    Add lockboxes solution and README file

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """Checks if all the boxes can be opened."""
+    if len(boxes) == 0:
+        return False
+    if type(boxes) is not list:
+        return False
+    if len(boxes) == 1:
+        return True
+    visited = [0]
+    for current_box in visited:
+        for key in boxes[current_box]:
+            if key not in visited and key < len(boxes):
+                visited.append(key)
+    if len(visited) == len(boxes):
+        return True
+    return False
