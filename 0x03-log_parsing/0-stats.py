24fde6447d945806f160a2f1e639282cad6a0165
kaleabendrias <kaleabendrias1212@gmail.com>
2024-01-25 09:52:01 +0300

    Update file permissions in 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,7 +4,14 @@ import sys
 
 
 def print_msg(dict_sc, total_file_size):
-    """print_msg"""
+    """
+    Method to print
+    Args:
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
+    """
 
     print("File size: {}".format(total_file_size))
     for key, val in sorted(dict_sc.items()):
@@ -26,8 +33,8 @@ dict_sc = {"200": 0,
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()
-        parsed_line = parsed_line[::-1]
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
         if len(parsed_line) > 2:
             counter += 1
