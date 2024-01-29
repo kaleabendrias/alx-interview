394662136a7d52f28c3b2a09592af0aef32096ef
kal <kaleabendrias1212@gmail.com>
2024-01-29 12:28:31 +0300

    initial push

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""method that determines if a given data set represents a valid UTF-8"""
+
+
+def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8"""
+    
