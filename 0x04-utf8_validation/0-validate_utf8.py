697afb8a828b72e0090e87e0c92589ef80226d37
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-01 10:38:35 +0300

    Refactor validUTF8 function to improve readability and efficiency

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,39 +3,24 @@
 
 
 def validUTF8(data):
-    """
-    Determines if a given data set represents a valid UTF-8 encoding.
-
-    Args:
-        data: A list of integers, each representing a byte of data.
-
-    Returns:
-        True if the data is a valid UTF-8 encoding, False otherwise.
-    """
-
-    num_bytes_remaining = 0
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    num_bytes = 0
 
     for num in data:
-        if num_bytes_remaining == 0:
-            # Check for single-byte characters
-            if (num >> 7) == 0:
+        if num_bytes == 0:
+            if (num >> 7) == 0b0:
                 continue
-
-            # Check for multi-byte characters
-            leading_ones = 0
-            while num & (1 << 7):  # Count leading 1 bits
-                leading_ones += 1
-                num <<= 1
-
-            if not (2 <= leading_ones <= 4):
-                return False  # Invalid leading byte
-
-            num_bytes_remaining = leading_ones - 1
-        else:
-            # Check for continuation bytes
-            if not (num >> 6) == 0b10:
+            elif (num >> 5) == 0b110:
+                num_bytes = 1
+            elif (num >> 4) == 0b1110:
+                num_bytes = 2
+            elif (num >> 3) == 0b11110:
+                num_bytes = 3
+            else:
                 return False
+        else:
+            if (num >> 6) != 0b10:
+                return False
+            num_bytes -= 1
 
-        num_bytes_remaining -= 1
-
-    return num_bytes_remaining == 0
+    return num_bytes == 0
