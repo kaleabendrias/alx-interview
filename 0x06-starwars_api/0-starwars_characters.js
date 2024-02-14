b7971c5fe35749867757b85bcdd4bd759f1c6e94
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-14 12:44:42 +0300

    Refactor character retrieval to use async/await

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,30 +6,28 @@ const movieId = process.argv[2];
 
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request(url, { json: true }, (err, res, body) => {
+request(url, { json: true }, async (err, res, body) => {
   if (err) {
-    return console.log(err);
+    return console.error(err);
   }
 
-  const characterPromises = body.characters.map((characterUrl) => {
-    return new Promise((resolve, reject) => {
-      request(characterUrl, { json: true }, (err, res, body) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(body.name);
-        }
-      });
-    });
-  });
-
-  Promise.all(characterPromises)
-    .then((characters) => {
-      characters.forEach((character) => {
-        console.log(character);
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  try {
+    const characters = await Promise.all(
+      body.characters.map(async (characterUrl) => {
+        const characterData = await new Promise((resolve, reject) => {
+          request(characterUrl, { json: true }, (err, res, body) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(body.name);
+            }
+          });
+        });
+        return characterData;
+      })
+    );
+    characters.forEach((character) => console.log(character));
+  } catch (error) {
+    console.error(error);
+  }
 });
