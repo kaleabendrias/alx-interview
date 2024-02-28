05010befed97cd7d63927144b9ab4c1dd88a137a
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-28 20:03:09 +0300

    Add makeChange function to calculate the fewest number of coins needed

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# determine the fewest number of coins needed to meet a given
+def makeChange(coins, total):
+    # Create a list to store the minimum number of coins needed
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
